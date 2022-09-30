9ae43b2e1b4219beae7f79896e630d46e69254c8
edi-sipka@gmail.com <edisipka@gmail.com>
2022-09-30 19:26:56 +0200

    Fetch Api data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,30 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = 'Harry';
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}%type=movie`)
+        .catch((err) => {
+          console.log('Err:', err);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img">
+        <MovieListing />
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default Home;
