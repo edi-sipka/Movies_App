084ee78b36be68795aedb920ebbd46c1e14c3235
edi-sipka@gmail.com <edisipka@gmail.com>
2022-10-03 20:10:17 +0200

    Add shows listing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,19 @@
 /* eslint-disable */
 import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
-import movieApi from '../../common/apis/movieApi';
-import { APIKey } from '../../common/apis/MovieApiKey';
+
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../features/movies/movieSlice';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice';
 
 const Home = () => {
-  const movieText = 'Taxi';
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi.get(
-        `?apiKey=${APIKey}&s=${movieText}&type=movie`
-      );
-      console.log(response).catch((err) => {
-        console.log('Err:', err);
-      });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img">
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,30 @@
 /* eslint-disable */
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
+export const fetchAsyncMovies = createAsyncThunk(
+  'movies/fetchAsyncMovies',
+  async () => {
+    const movieText = 'Taxi';
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&s=${movieText}&type=movie`
+    );
 
+    return response.data;
+  }
+);
+
+export const fetchAsyncShows = createAsyncThunk(
+  'movies/fetchAsyncShows',
+  async () => {
+    const seriesText = 'Friends';
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&s=${seriesText}&type=series`
+    );
+    return response.data;
+  }
+);
 const initialState = {
   movies: {},
 };
@@ -14,8 +37,26 @@ const movieSlice = createSlice({
       state.movies = payload;
     },
   },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log('Pending');
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log('Fetched');
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log('Rejected');
+    },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log('Fetched Successfully!');
+      return { ...state, shows: payload };
+    },
+  },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+
 export default movieSlice.reducer;
